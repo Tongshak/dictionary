7c35b2496bf0dc52036fb15e39ca6eec1925e770
Tongshak <sabotongshak225@gmail.com>
2025-07-30 13:24:15 +0000

    dictionary

---
+++ exercise_1.py
@@ -0,0 +1,11 @@
+response = {
+#key value
+"success": True,
+"message": "login successful!",
+"data": [
+     "example@mail.com",
+      "bravos"
+   ]
+}
+print (response)
+print(response["success"])
