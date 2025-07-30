4a8be0965c0f68313fa4aade3b4fda31d6f27a62
Tongshak <sabotongshak225@gmail.com>
2025-07-30 13:36:43 +0000

    my oxford dictionary

---
+++ exercise_2.py
@@ -0,0 +1,19 @@
+oxford={
+  "apple": "a common fruit",
+   "ball": "a round ojject",
+   "cat": "an animal of the family felidae",
+   "dog": "my dog meat",
+   "egg": "a pherical object",
+    "fish": "a cold bloodedvertebrate",
+    "goat": "modric",
+    "house": "a building",
+     "idiot": "wild animal",
+      "jacket":"wild animal",
+}
+#print(dictionary, type(dictionary))
+#print("meaning of dictionary " + oxford["ball"])
+oxford["kettle"] = "a vessel for boiling"
+oxford["jacka1"] = "a wild animal in the bush"
+#print(oxford)
+#print(oxford.keys(), type(oxford.keys()))
+print(oxford.keys(), type(oxford.values()))
